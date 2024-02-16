eb3347f511559a6adf6d15e383b7a11756eab496
Louise <Louise.blanc@chasacademy.se>
2024-02-16 14:12:12 +0000

    Blade navbar

--- u05laravel/resources/views/navbar.blade.php
+++ u05laravel/resources/views/navbar.blade.php
@@ -6,8 +6,8 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
+  <div class="text-3xl font-bold underline">
     Hello world!
-  </h1>
+</div>
 </body>
 </html>
