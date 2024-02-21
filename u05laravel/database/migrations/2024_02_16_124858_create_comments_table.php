24a028d7cbff7306926b17263e96ff582d2c54b4
Mikael Jakobsson <Maadmike@hotmail.se>
2024-02-21 16:12:56 +0000

    comment update

--- u05laravel/database/migrations/2024_02_16_124858_create_comments_table.php
+++ u05laravel/database/migrations/2024_02_16_124858_create_comments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('comment');
             $table->integer('rating')->nullable();
             $table->timestamps();
         });
