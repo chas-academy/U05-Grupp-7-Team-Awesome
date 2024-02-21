fba88d3ac7e78e195f821a7b8b05eddfe3431209
Cissie Andersson <cissie.andersson@chasacademy.se>
2024-02-21 10:34:36 +0000

    first

---
+++ u05laravel/resources/views/auth/scifi.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sci-fi films</title>
+</head>
+<body>
+<h1>Sci-fi films</h1>
+<div class="grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-3 gap-8">
+    <a href="#">
+        <img src="u05laravel/public/seed-moviecover/Aeonflux.jpg" alt="Aeon Flux" class="hover:opacity-75 transition
+        ease-in-out duration-150">
+    </a>
+    <a href="#">
+        <img src="u05laravel/public/seed-moviecover/Intersteller.jpg" alt="Interstellar" class="hover:opacity-75 transition
+        ease-in-out duration-150">
+    </a>
+    <a href="#">
+        <img src="u05laravel/public/seed-moviecover/Matrix.jpg" alt="Matrix" class="hover:opacity-75 transition
+        ease-in-out duration-150">
+    </a>
+
+</div>
+    
+</body>
+</html>
