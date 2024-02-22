7cdb001bf0995c2a754cceb777800dd2987f4953
mohamed abdi <mgudaal6@gmail.com>
2024-02-22 08:32:24 +0000

    update footer

--- u05laravel/resources/views/layouts/navigation.blade.php
+++ u05laravel/resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('setting') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
