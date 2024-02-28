0a7aec297fa4d9a423384cf3c9b8b74528d1776a
mohamed abdi <mgudaal6@gmail.com>
2024-02-28 10:54:18 +0000

    fixed på table design lyckade small screen att ska funkar på sidan

--- u05laravel/resources/views/genre.blade.php
+++ u05laravel/resources/views/genre.blade.php
@@ -28,60 +28,66 @@
 
 
 <body>
+<div class="flex flex-col min-h-screen items-center justify-center bg-gray-100 dark:bg-white-900 mx-auto">
+    <div class="container mx-auto px-4 sm:px-6 lg:px-8 mx-auto">
 
-    <div class="flex flex-col min-h-screen items-start justify-center bg-gray-100 dark:bg-white-900 mx-auto">
-        <div class="flex flex-col min-h-screen bg-gray-100 dark:bg-white-900 mx-auto">
-            <div class="container mx-auto px-4 sm:px-6 lg:px-8 mx-auto">
+        <!-- Heading -->
+        <h1 class="text-3xl mb-8 flex justify-center" style="color: #ff0000;">Genre Movies</h1>
 
-                <!-- Heading -->
-                <h1 class="text-3xl mb-8 flex justify-center">Genre Movies</h1>
+        <!-- Filter Form -->
+        <form action="{{ route('genre.filter') }}" method="GET" class="mb-8 flex flex-col sm:flex-row items-center justify-center">
+    <label for="genre" class="mr-2">Select Genre:</label>
+    <div class="flex">
+        <select name="genre" id="genre" class="flex-grow bg-gray-300 text-black p-2 mb-2 sm:mb-0 mr-2 rounded-md border border-gray-300 focus:outline-none">
+            <option value="">All</option>
+            @foreach ($genres as $genre)
+            <option value="{{ $genre }}">{{ $genre }}</option>
+            @endforeach
+        </select>
+        <button type="submit" class="bg-gray-300 text-black px-4 py-2 rounded-md border border-gray-300 focus:outline-none">Filter</button>
+    </div>
+</form>
 
-                <!-- Filter Form -->
-                <form action="{{ route('genre.filter') }}" method="GET" class="mb-8 flex flex-col sm:flex-row items-center flex justify-center">
-                    <label for="genre" class="mr-4">Select Genre:</label>
-                    <select name="genre" id="genre" class="rounded-md bg-gray-500 text-black p-2 mb-2 sm:mb-0">
-                        <option value="">All</option>
-                        @foreach ($genres as $genre)
-                        <option value="{{ $genre }}">{{ $genre }}</option>
-                        @endforeach
-                    </select>
-                    <button type="submit" class="bg-gray-500 text-black px-4 py-2 rounded-md ml-0 sm:ml-4">Filter</button>
-                </form>
 
-                <!-- Responsive Table -->
-                <div class="overflow-x-auto flex justify-center">
-                    <table class="w-full sm:w-full md:w-4/5 lg:w-3/4 xl:w-2/3 bg-gray border-collapse border border-gray-300 sm:rounded-lg">
-                        <thead class="hidden sm:table-header-group">
-                            <tr class="hover:bg-gray-100 sm:table-row flex flex-col w-full">
-                                <th class="px-6 py-3 text-left sm:w-1/6">Title</th>
-                                <th class="px-6 py-3 text-left sm:w-1/6">Genre</th>
-                                <th class="px-6 py-3 text-left sm:w-1/6">Country</th>
-                                <th class="px-6 py-3 text-left sm:w-1/6">Release Year</th>
-                                <th class="px-6 py-3 text-left sm:w-1/6">Director</th>
-                                <th class="px-6 py-3 text-left sm:w-1/6">Photo</th>
-                                <th class="px-6 py-3 text-left sm:w-1/6">Comment</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($movies as $movie)
-                            <tr class="hover:bg-gray-100 sm:table-row flex flex-col w-full">
-                                <td class="border px-6 py-3 sm:w-1/6">{{ $movie->titel }}</td>
-                                <td class="border px-6 py-3 sm:w-1/6">{{ $movie->genre }}</td>
-                                <td class="border px-6 py-3 sm:w-1/6">{{ $movie->country }}</td>
-                                <td class="border px-6 py-3 sm:w-1/6">{{ $movie->year }}</td>
-                                <td class="border px-6 py-3 sm:w-1/6">{{ $movie->director }}</td>
-                                <td class="border px-6 py-3 sm:w-1/6"><img src="{{ asset($movie->photoPath) }}" alt="{{ $movie->title }}" class="w-16 h-16"></td>
-                                <td class="border px-6 py-3 sm:w-1/6">
-                                    <a href="{{ url('/comment/'.$movie->id) }}">Comment</a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+
+
+        <!-- Responsive Table -->
+        <div class="overflow-x-auto flex justify-center">
+            <table class="w-full sm:w-full md:w-4/5 lg:w-3/4 xl:w-2/3 bg-gray border-collapse border border-gray-300 sm:rounded-lg">
+                <thead class="hidden sm:table-header-group">
+                    <tr class="bg-gray-300 sm:w-full">
+                    <th class="py-2 px-4 border-b sm:table-cell">ID</th>
+                         <th class="py-2 px-4 border-b sm:table-cell">Title</th>
+                        <th class="py-2 px-4 border-b sm:table-cell">Title</th>
+                        <th class="py-2 px-4 border-b sm:table-cell">Genre</th>
+                        <th class="py-2 px-4 border-b sm:table-cell">Country</th>
+                        <th class="py-2 px-4 border-b sm:table-cell">Release Year</th>
+                        <th class="py-2 px-4 border-b sm:table-cell">Director</th>
+                        <th class="py-2 px-4 border-b sm:table-cell">Photo</th>
+                        <th class="py-2 px-4 border-b sm:table-cell">Comment</th>
+                    </tr>
+                </thead>
+                <tbody class="sm:table-row-group">
+                    @foreach ($movies as $movie)
+                    <tr class="hover:bg-gray-100 sm:table-row flex flex-col w-full">
+                    <td class="py-2 px-4 border-b sm:table-cell">{{ $movie->id }}</td>
+                        <td class="py-2 px-4 border-b sm:table-cell">{{ $movie->titel }}</td>
+                        <td class="py-2 px-4 border-b sm:table-cell">{{ $movie->genre }}</td>
+                        <td class="py-2 px-4 border-b sm:table-cell">{{ $movie->country }}</td>
+                        <td class="py-2 px-4 border-b sm:table-cell">{{ $movie->year }}</td>
+                        <td class="py-2 px-4 border-b sm:table-cell">{{ $movie->director }}</td>
+                        <td class="py-2 px-4 border-b sm:table-cell"><img src="{{ asset($movie->photoPath) }}" alt="{{ $movie->title }}" class="w-16 h-16"></td>
+                        <td class="py-2 px-4 border-b sm:table-cell">
+                            <a href="{{ url('/comment/'.$movie->id) }}">Comment</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-</body>
+    </div>
+</div>
+
 
 <div class=" w-full bg-gray-800 text-white p-4 z-50 block">
     @include('footer')
